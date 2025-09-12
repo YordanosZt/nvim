d800b11976b75e8e0289d4614ba9ca01d66ba802
Yordanos <yordanoszt15@gmail.com>
2025-09-12 17:08:03 +0300

    revert keymaps to original

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -45,11 +45,11 @@ vim.keymap.set('n', '<Left>', ':vertical resize -2<CR>', opts)
 vim.keymap.set('n', '<Right>', ':vertical resize +2<CR>', opts)
 
 -- Buffers
---vim.keymap.set('n', '<Tab>', ':bnext<CR>', opts)
---vim.keymap.set('n', '<S-Tab>', ':bprevious<CR>', opts)
---vim.keymap.set('n', '<C-i>', '<C-i>', opts) -- to restore jump forward
---vim.keymap.set('n', '<leader>x', ':Bdelete!<CR>', opts) -- close buffer
---vim.keymap.set('n', '<leader>b', '<cmd> enew <CR>', opts) -- new buffer
+vim.keymap.set('n', '<Tab>', ':bnext<CR>', opts)
+vim.keymap.set('n', '<S-Tab>', ':bprevious<CR>', opts)
+vim.keymap.set('n', '<C-i>', '<C-i>', opts) -- to restore jump forward
+vim.keymap.set('n', '<leader>x', ':bdelete<CR>', opts) -- close buffer
+vim.keymap.set('n', '<leader>b', '<cmd> enew <CR>', opts) -- new buffer
 
 -- Increment/decrement numbers
 vim.keymap.set('n', '<leader>+', '<C-a>', opts) -- increment
@@ -68,15 +68,15 @@ vim.keymap.set('n', '<C-h>', ':wincmd h<CR>', opts)
 vim.keymap.set('n', '<C-l>', ':wincmd l<CR>', opts)
 
 -- Tabs
---vim.keymap.set('n', '<leader>to', ':tabnew<CR>', opts) -- open new tab
---vim.keymap.set('n', '<leader>tx', ':tabclose<CR>', opts) -- close current tab
---vim.keymap.set('n', '<leader>tn', ':tabn<CR>', opts) --  go to next tab
---vim.keymap.set('n', '<leader>tp', ':tabp<CR>', opts) --  go to previous tab
+vim.keymap.set('n', '<leader>to', ':tabnew<CR>', opts) -- open new tab
+vim.keymap.set('n', '<leader>tx', ':tabclose<CR>', opts) -- close current tab
+vim.keymap.set('n', '<leader>tn', ':tabn<CR>', opts) --  go to next tab
+vim.keymap.set('n', '<leader>tp', ':tabp<CR>', opts) --  go to previous tab
 
-vim.keymap.set('n', '<leader>n', ':tabnew<CR>', opts) -- open new tab
-vim.keymap.set('n', '<leader>x', ':tabclose<CR>', opts) -- close current tab
-vim.keymap.set('n', '<Tab>', ':tabn<CR>', opts) --  go to next tab
-vim.keymap.set('n', '<S-Tab>', ':tabp<CR>', opts) --  go to previous tab
+--vim.keymap.set('n', '<leader>n', ':tabnew<CR>', opts) -- open new tab
+--vim.keymap.set('n', '<leader>x', ':tabclose<CR>', opts) -- close current tab
+--vim.keymap.set('n', '<Tab>', ':tabn<CR>', opts) --  go to next tab
+--vim.keymap.set('n', '<S-Tab>', ':tabp<CR>', opts) --  go to previous tab
 
 -- Toggle line wrapping
 vim.keymap.set('n', '<leader>lw', '<cmd>set wrap!<CR>', opts)
